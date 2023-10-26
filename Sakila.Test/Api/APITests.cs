b7b988da3b39784d96a6baae50569830ac713b0b
blakeneybk <blakeneybk@gmail.com>
2023-10-25 17:30:00 -0700

    API test acknowledgement

---
+++ Sakila.Test/Api/APITests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sakila.Test.Api
+{
+    internal class APITests
+    {
+        //I just wanted to acknowledge that there should probably be API integration tests here or possibly even in a separate project entirely.
+        //I cannot do them due to time constraints and I have already spent quite a bit of time on this project. I hope you understand.
+    }
+}
